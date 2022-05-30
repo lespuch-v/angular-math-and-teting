dc333fa36bb4956bbcf010ff56b7c399fc1c645b
Vasyl Lespuch <36127590+lespuch-v@users.noreply.github.com>
2022-05-30 13:49:54 +0200

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -16,16 +15,92 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'math-test'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('math-test');
-  });
-
-  it('should render title', () => {
+  it('should render math-test', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('math-test app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('math-test');
   });
+});
+
+describe('AppComponent - input', () => {
+  it('text input should ber rendered', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('input')).toBeTruthy();
+  });
+  it('label should be rendered', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('label')).toBeTruthy();
+    expect(compiled.querySelector('label')?.textContent).toContain('Number:');
+  });
+});
+
+describe(' AppComponent - button testing', () => {
+  it("button 'Double' should be rendered", () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelectorAll('button')[2]?.textContent).toContain(
+      'Double'
+    );
+  });
+  it("button 'Even / Odd' should be rendered", () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelectorAll('button')[1]?.textContent).toContain(
+      'Even / Odd'
+    );
+  });
+  it("button 'Root' should be rendered", () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    const de = fixture.debugElement.query(By.css('.btnStyled'));
+    expect(de.classes['btnStyled']).toBeTruthy();
+    expect(compiled.querySelectorAll('button')[0]?.textContent).toContain(
+      'Root'
+    );
+  });
+  it("button 'Reset' should be rendered", () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelectorAll('button')[3]?.textContent).toContain(
+      'Reset'
+    );
+  });
+});
+
+// ---------------------------
+describe('AppComponent - TESTING methods', () => {
+  let component: AppComponent;
+  beforeEach(async () => {
+    component = new AppComponent()
+    await TestBed.configureTestingModule({
+      declarations: [AppComponent],
+
+    }).compileComponents();
+  });
+  it('Reset Button - should set input to 0 and header to math-test', () => {
+    component.reset();
+    expect(component.title).toContain('math-test');
+    expect(component.userInput).toBe(0)
+  });
+  it("Double Button - should be double 2 => 4", ()=>{
+    let num = 2
+    component.userInput = num
+    component.double()
+    expect(component.title).toContain("4")
+  })
+  it("Root Button - should ROOT the values", ()=>{
+    let num = 256
+    component.userInput = num
+    component.double()
+    expect(component.title).toContain("16")
+  })
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'math-test';
+  userInput: number = 0;
+
+  handleInput(event: any) {
+    this.userInput = event.target.value;
+    console.log(this.userInput);
+  }
+  double() {
+    const dbl = Number(this.userInput * this.userInput)
+    this.title = String(dbl)
+  }
+  reset(){
+    this.title = "math-test"
+    this.userInput = 0
+  }
+  root(){
+    let rt = Math.sqrt(Number(this.userInput))
+    console.log(rt)
+    this.title = String(rt)
+  }
+  evenOrOdd(){
+    const num = this.userInput
+    if(num %2 ==0){
+      this.title = "Even"
+    }else{
+      this.title = "Odd"
+    }
+  }
+
 }
